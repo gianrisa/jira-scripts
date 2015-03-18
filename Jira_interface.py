8fe4be2e3a64185ea3582f401a637202b8370f7b
gianfranco risaliti <gianfranco.risaliti@gmail.com>
2015-03-18 15:01:37 +0100

    Added method work in progress moved dict outside

--- Jira_interface.py
+++ Jira_interface.py
@@ -3,7 +3,8 @@ __author__ = 'risalgia'
 
 from jira.client import JIRA
 import pprint
-
+from jira_field_entity import issue_mapping_old_new, fields_mapping_old_new, standard_minimum_defect, standard_minimum_uti, standard_minimum_epic, new_standard_fields, old_project_all_fields_all_issues
+from secret_file import secret_new, secret_old
 import time                                                
 
 def timeit(method):
@@ -15,87 +16,6 @@ def timeit(method):
               (method.__name__, args, kw, te-ts)
         return result
     return timed
-
-
-issue_mapping_old_new = {
-    "Data Quality"      : "Defect",
-    "Performance"       : "Defect",
-    "Change Request"    : "Improvement",
-    "Functional defect" : "Defect",
-    "UI"                : "Defect"
-    }
-
-fields_mapping_old_new = {
-    "priority"          : "priority",
-    "project"           : "project",
-    "summary"           : "summary",
-    "description"       : "description",
-    "issuetype"         : "issuetype",
-    "reporter"          : "reporter"
-}
-
-
-standard_minimum_defect = """{  'customfield_10001': %(phase)s,
-                                'customfield_10022': %(Severity)s,
-                                'description'      : %(description)s,
-                                'environment'      : %(environment)s,
-                                'issuetype'        : {'name': %(issuetype)s},
-                                'project'          : {'key' : %(project)s},
-                                'reporter'         : %(reporter)s,
-                                'summary'          : %(summary)s
-                            }"""
-
-standard_minimum_uti = """{ 'description': %(description)s,
-                            'issuetype'  : {'name': %(issuetype)s},
-                            'project'    : {'key' : %(project)s},
-                            'reporter'   : %(reporter)s,
-                            'summary'    : %(summary)s
-                          }"""
-
-
-standard_minimum_epic = """{  'customfield_10014': 'Epic Name',
-                               'description' : %(description)s,
-                               'issuetype'   : {'name': %(issuetype)s},
-                               'project'     : {'key' : %(project)s},
-                               'reporter'    : %(reporter)s,,
-                               'summary'     :  %(summary)s
-                            }"""
-
-new_standard_fields = {'Defect': {  'customfield_10001': 'Phase',
-                                     'customfield_10022': 'Severity',
-                                     'description': 'Description',
-                                     'environment': 'Environment',
-                                     'issuetype': 'Issue Type',
-                                     'project': 'Project',
-                                     'reporter': 'Reporter',
-                                     'summary': 'Summary'},
-                       'EPIC': {  'customfield_10014': 'Epic Name',
-                                   'description': 'Description',
-                                   'issuetype': 'Issue Type',
-                                   'project': 'Project',
-                                   'reporter': 'Reporter',
-                                   'summary': 'Summary'},
-                       'Improvement': {  'description': 'Description',
-                                          'issuetype': 'Issue Type',
-                                          'project': 'Project',
-                                          'reporter': 'Reporter',
-                                          'summary': 'Summary'},
-                       'Sub-task': {  'description': 'Description',
-                                       'issuetype': 'Issue Type',
-                                       'parent': 'Parent',
-                                       'project': 'Project',
-                                       'reporter': 'Reporter',
-                                       'summary': 'Summary'},
-                       'Task': {  'description': 'Description',
-                                   'issuetype': 'Issue Type',
-                                   'project': 'Project',
-                                   'reporter': 'Reporter',
-                                   'summary': 'Summary'},
-                       'User Story': {  'description': 'Description',
-                                         'issuetype': 'Issue Type',
-                                         'project': 'Project',
-                                         'reporter': 'Reporter',
-                                         'summary': 'Summary'}}
 
 
 # jira connector
@@ -115,33 +35,31 @@ def jissue_get_last(j_old, project_old):
     j_issue_max_key = int(j_issues_old_count[0].key.split("-")[1])
     return j_issue_max_key
 
-@timeit
+
 def jissue_get_fields(jira_dict):
     fields = jira_dict['projects'][0]['issuetypes'][0]['fields']    #pprint(fields,indent=2)
-    issue_id = jira_dict['projects'][0]['issuetypes'][0]['id']
-    project_id = {'id':jira_dict['projects'][0]['id']}
-    project_key = {'key':str(jira_dict['projects'][0]['key'])}
-
-    custom_fields_map = {}
-    all_fields_map = {}
-    required_map = {}
+    required_fields_map = all_fields_map = {}
 
     for field in fields:
-        if fields[field].has_key('required'):
-            if fields[field]['required']:
-                required_map.update({field:fields[field]['name']})
-            custom_fields_map.update({field:fields[field]['name']})
-    return required_map
+        if fields[field].has_key('required') and fields[field]['required']:
+            required_fields_map.update({fields[field]['name']:field})
+        else:
+            all_fields_map.update({fields[field]['name']:field})
+    return required_fields_map, all_fields_map
 
-@timeit
-def jissue_get_mandatory_fields(jira, project):
-    issuetype_mandatory = {}
+
+def jissue_get_issuetype_fields(jira, project, mandatory_only=False):
+    issuetype_fields = {}
     jproject = jira.project(project)
+
     for i in jproject.issueTypes:
         j_issue_dict = jira.createmeta(project, issuetypeIds=i.id, expand='projects.issuetypes.fields')
-        required_map = jissue_get_fields(j_issue_dict)
-        issuetype_mandatory.update({i.name:required_map})
-    return issuetype_mandatory
+        required_map, all_fields_map = jissue_get_fields(j_issue_dict)
+        issuetype_fields.update({i.name:required_map})
+        if not mandatory_only:
+            issuetype_fields.update({i.name:all_fields_map})
+    return issuetype_fields
+
 
 @timeit
 # this method is used to get the issue list with references, in case the number of issues is more than 1000
@@ -149,8 +67,8 @@ def jissue_get_chunked(jira, project, issue_max_count, chunks=100):
     # list for result.
     result = []
     # step and rest simple calc
-    step = issue_max_count/chunks
-    rest = issue_max_count%chunks
+    step = issue_max_count / chunks
+    rest = issue_max_count % chunks
     # iterate the issue gathering
     for i in range(step):
         result.extend(jissue_query(jira, project, chunks*i, chunks))
@@ -163,18 +81,31 @@ if __name__ == "__main__":
 # first stage issue creation in project from project reference.
 
     # connect to the old jira server parameters
-    j_old_param = ('https://tools.adidas-group.com/jiraold','username','password')
+    j_old_param = ('https://tools.adidas-group.com/jiraold', secret_old[0], secret_old[1])
     # the project name shall be given as external parameter
-    project = "DIT"
+    project_old = "DIT"
     # perform jira connection
     j_old = jconnect(j_old_param)
+
+    # now we need the mapping the ugly mapping, then:
+    # get the fields that are mandatory and which are those that are used in the project and in the used issued.
+    # get a json and create a mapping, the source of the mapping is something like this:
+    # 
+    # mandatory_fiels = { Bug : {   field_project_old : %(field_project_new)s,
+    #                               field_project_old : %(field_project_new)s
+    #                    }  }
+    all_issue_fields = jissue_get_issuetype_fields(j_old, project_old)
+
+    pprint.pprint(all_issue_fields, indent=3)
+
+
     # get the max issues and the issue list - issue contains the issue pointer to the old jira - it can be used to check the needed data.
     # to be migrated like .. links .. attachment .. status.. this is pretty big anyway 
-    issue_max_key =  jissue_get_last(j_old, project)    
+    issue_max_key =  jissue_get_last(j_old, project_old)    
     # get all the issues in the project pretty expensive operation, this will take a while
-    issues_old = jissue_get_chunked(j_old, project, issue_max_key)
+    issues_old = jissue_get_chunked(j_old, project_old, issue_max_key)
     # list older for the issues that need to be created in jira_new empty vector
-    issues_list = ['']*issue_max_key+1
+    issues_list = ['']*(issue_max_key+1)
     
     # place the value in the right position
     for issue in issues_old:
@@ -185,9 +116,25 @@ if __name__ == "__main__":
         # create the issues in jira with reference list
         if issue:
             print "copy from the old jira : - ", issue
+
+            import pdb; pdb.set_trace()
+
+  
         else:
             print "create dummy issue : - ", issue
+    
+
+
+    # now we need the mapping the ugly mapping, then:
+        # get the fields that are mandatory and which are those that are used in the project and in the used issued.
+        # get a json and create a mapping, the source of the mapping is something like this:
+        # 
+        # mandatory_fiels = { Bug : {   field_project_old : %(field_project_new)s,
+        #                               field_project_old : %(field_project_new)s
+        #                    }  }
+        # 
         
+
         # issues_list.append(jira.create_issue(fields={'project':{'key': i.project},
         #                                 'summary':i.summary,
         #                                    'description':'',
@@ -195,12 +142,11 @@ if __name__ == "__main__":
         #                                   }, prefetch=True))
 
     # jira new server parameters
-    j_new_param = ('https://tools.adidas-group.com/jira','username','password')  
+    j_new_param = ('https://tools.adidas-group.com/jira', secret_old[0], secret_old[1])  
     # connect to the new jira and create the issues with empty dummy values
-    project = "DIT"
+    project_new = "DIT"
     # perform jira connection
     j_new = jconnect(j_new_param)
-
 
 
 def jissue_filed_mapping_controller(j_new, j_old, project_new, project_old, issue_old, issue_new):
